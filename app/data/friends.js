aa5ada000b640f33186fe34d34c601d2b195db23
Stephanie <mtfishergrl@hotmail.com>
2018-05-06 14:00:49 -0600

    html fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,25 @@ let existingUsers = [];
 
 let incomingUser = {};
 
-$(".button").on("click", function() {
+$("#subButton").on("click", function() {
   event.preventDefault();
+  console.log("one");
   let questions = $('.dropdownMenuButton');
   let answers = [];
+  console.log("two");
   for (var i=0; i<questions.length; i++) {
     let values = dropdown-menu.val();
     answers.push(values);
     
   }
+  console.log("three");
+  
   incomingUser = {
     name: $('#name').val().trim(),
     picture: $('#picture').val().trim(),
     answers: answers
   };
-  
+  console.log(incomingUser);
 });
+
+console.log(incomingUser);
