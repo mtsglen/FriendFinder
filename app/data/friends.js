e0e14a981fb8d3dc8499478036acc7612964792c
Stephanie Glendenning <mtfishergrl@hotmail.com>
2018-05-12 10:36:54 -0600

    fixed image

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,14 @@
 let existingUsers = [
   {
     "name":"Ahmed",
-    "picture":"https://en.wikipedia.org/wiki/Puppy#/media/File:Golde33443.jpg",
+    "picture":"http://petpuppymania.com/images/back1.jpg",
     "scores":[
       5, 1, 4, 4, 5, 1, 2, 5, 4, 1
       ]
   },
   {
     "name":"John",
-    "picture":"https://en.wikipedia.org/wiki/Puppy#/media/File:Cara_de_quem_caiu_do_caminh%C3%A3o..._(cropped).jpg",
+    "picture":"https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2017/10/terrier-puppy.jpg?itok=rIgh3ArV&fc=50,50",
     "scores":[
       1, 1, 1, 1, 1, 1, 1, 1, 1, 1
       ]
