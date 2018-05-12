a66d34c969a4d65fbee71808df2a56cc48a46b91
Stephanie Glendenning <mtfishergrl@hotmail.com>
2018-05-11 21:49:07 -0600

    ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # FriendFinder
+
+This project was designed to match a person to an existing user by closest match with common interests.
+
+## Build Status
+
+This build is 98% complete.  Some tags are broken and still under construction.
+
+## Code style
+This product is built using javascript, node, and express.
+
+## Additional Items
+My app is hosted on Heroku at https://rocky-harbor-80731.herokuapp.com/
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,14 @@
 let existingUsers = [
   {
     "name":"Ahmed",
-    "photo":"https://en.wikipedia.org/wiki/Puppy#/media/File:Golde33443.jpg",
+    "picture":"https://en.wikipedia.org/wiki/Puppy#/media/File:Golde33443.jpg",
     "scores":[
       5, 1, 4, 4, 5, 1, 2, 5, 4, 1
       ]
   },
   {
     "name":"John",
-    "photo":"https://en.wikipedia.org/wiki/Puppy#/media/File:Cara_de_quem_caiu_do_caminh%C3%A3o..._(cropped).jpg",
+    "picture":"https://en.wikipedia.org/wiki/Puppy#/media/File:Cara_de_quem_caiu_do_caminh%C3%A3o..._(cropped).jpg",
     "scores":[
       1, 1, 1, 1, 1, 1, 1, 1, 1, 1
       ]
